6970c87a0d4b0bf64441910248efb43e2388c612
zhukata <zhukata@gmail.com>
2025-06-26 19:24:52 +0300

    two step

--- ___tests__/pageLoader.test.js
+++ ___tests__/pageLoader.test.js
@@ -3,12 +3,14 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import { mkdtemp, readFile } from 'fs/promises'
 import nock from 'nock'
-import pageLoader from '../src/index.js'
+import { pageLoader, pictureLoader } from '../src/index.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+const getFixturePath = name => path.join(__dirname, '..', '__fixtures__', name)
+
 const testUrl = 'https://ru.hexlet.io/courses'
-const testHTML = '<html><head><title>Test</title></head><body>Hello</body></html>'
+const testHTML = await readFile(getFixturePath('test.html'), 'utf-8')
 
 let tempDir
 
@@ -26,10 +28,31 @@ test('downloads page and saves correctly', async () => {
     .reply(200, testHTML)
 
   const outputPath = await pageLoader(testUrl, tempDir)
-  const expectedPath = path.join(tempDir, 'ru-hexlet-io-courses.html')
+  const expectedPath = path.join(tempDir, 'ru-hexlet-io-courses_files/ru-hexlet-io-courses.html')
 
   expect(outputPath).toBe(expectedPath)
 
   const savedContent = await readFile(expectedPath, 'utf-8')
   expect(savedContent).toBe(testHTML)
+})
+
+test('downloads picture and saves it correctly', async () => {
+  const binaryFixture = await readFile(getFixturePath('picture.png'))
+
+  // Имитация HTML с <img>
+  nock('https://ru.hexlet.io')
+    .get('/courses')
+    .reply(200, '<img src="/assets/professions/nodejs.png">')
+
+  // Имитация самой картинки
+  nock('https://ru.hexlet.io')
+    .get('/assets/professions/nodejs.png')
+    .reply(200, binaryFixture, {
+      'Content-Type': 'image/png',
+    })
+
+  const savedPicturePath = await pictureLoader(testUrl, tempDir)
+  const savedBuffer = await readFile(savedPicturePath)
+
+  expect(savedBuffer.equals(binaryFixture)).toBe(true)
 })
--- src/index.js
+++ src/index.js
@@ -2,22 +2,57 @@ import axios from 'axios'
 import fsp from 'fs/promises'
 import { URL } from 'url'
 import path from 'path'
+import * as cheerio from 'cheerio'
 
-export default (link, dirname) => {
-  const file = pathConstructor(link, dirname)
-  return axios.get(link)
-    .then(response => fsp.writeFile(file, response.data))
-    .then(() => { return file })
+const pageLoader = (link, dirname) => {
+  const url = new URL(link)
+  const { newDirPath, fullPath } = pathConstructor(url, dirname, 'html')
+  return fsp.mkdir(newDirPath, { recursive: true })
+    .then(() => axios.get(link))
+    .then(response => fsp.writeFile(fullPath, response.data))
+    .then(() => fullPath)
 }
 
-const pathConstructor = (link, dirname) => {
-  const url = new URL(link)
+const pathConstructor = (url, dirname, extension) => {
   const fullUrl = `${url.host}${url.pathname}`
   const normalized = fullUrl
-    .replace(/[^a-zA-Z0-9]/g, '-') // заменяем всё, кроме букв и цифр, на '-'
-    .replace(/-+/g, '-') // избавляемся от повторяющихся дефисов
-    .replace(/(^-|-$)/g, '') // удаляем дефисы в начале и конце строки
+    .replace(/[^a-zA-Z0-9]/g, '-')
+    .replace(/-+/g, '-')
+    .replace(/(^-|-$)/g, '')
 
-  const fileName = `${normalized}.html`
-  return path.join(dirname, fileName)
+  const newDirName = `${normalized}_files`
+  const fileName = `${normalized}.${extension}`
+
+  const newDirPath = path.join(dirname, newDirName)
+  const fullPath = path.join(newDirPath, fileName)
+
+  return { newDirPath, fullPath }
 }
+
+const pictureLoader = (link, dirname) => {
+  const url = new URL(link)
+  const { newDirPath, fullPath } = pathConstructor(url, dirname, 'png')
+
+  return fsp.mkdir(newDirPath, { recursive: true })
+    .then(() =>
+      axios.get(link).then(res => downloadPicture(res.data, fullPath, url))
+    )
+    .then(() => fullPath)
+}
+
+const downloadPicture = (html, pathToDownload, url) => {
+  const $ = cheerio.load(html)
+  const imageUrl = $('img').attr('src')
+  if (!imageUrl) {
+    return Promise.reject(new Error('Image URL not found'))
+  }
+
+  const absoluteUrl = new URL(imageUrl, url).toString()
+  return axios.get(absoluteUrl, { responseType: 'arraybuffer' })
+    .then(response =>
+      fsp.writeFile(pathToDownload, Buffer.from(response.data, 'binary'))
+    )
+    .then(() => pathToDownload)
+}
+
+export { pageLoader, pictureLoader }
